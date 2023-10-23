1775d3571037e49088758ee32d8703fff81fd27a
Achmad Nur Iqbal <ahmad.ibam13@gmail.com>
2023-10-23 13:38:06 +0700

    Update Terbaru

--- cypress/e2e/3-Web-Testing-Luma/CustomerLogin.cy.js
+++ cypress/e2e/3-Web-Testing-Luma/CustomerLogin.cy.js
@@ -1,23 +1,25 @@
-describe('Web UI Magento Luma', () => {
+import loginPage from "../../support/pageObjects/loginPage"
+
+describe('Verify Login', () => {
     it('Failed Login Magento Luma - Email does not match', () => {
         cy.visit('https://magento.softwaretestingboard.com/customer/account/login')
-        cy.get('#email').type('Iqbal')
-        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('password')
-        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click()
+        cy.get(loginPage.emailAddressField).type('Iqbal')
+        cy.get(loginPage.passwordField).type('password')
+        cy.get(loginPage.signinBtn).click()
         //cy.get('.mege-error').should('contain.text', 'Please enter a valid email address (Ex: johndoe@domain.com).') 
     })
-    it('Failed Login Magento Luma - password does not match', () => {
+    it('Failed Login Magento Luma - Password does not match', () => {
         cy.visit('https://magento.softwaretestingboard.com/customer/account/login')
         cy.get('#email').type('achmadnur.iqballll@gmail.com')
-        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('1234')
-        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click()
+        cy.get('#pass').type('1234')
+        cy.get('#send2 > span').click()
         cy.get('.message-error > div').should('contain.text', 'The account sign-in was incorrect or your account is disabled temporarily. Please wait and try again later.') 
     })
     it('Success Login Magento Luma', () => {
         cy.visit('https://magento.softwaretestingboard.com/customer/account/login')
         cy.get('#email').type('achmadnur.iqballll@gmail.com')
-        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').type('Qwerty1234@')
-        cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click()
+        cy.get('#pass').type('Qwerty1234@')
+        cy.get('#send2 > span').click()
         cy.get('.title').should('be.visible') 
     })
 })
